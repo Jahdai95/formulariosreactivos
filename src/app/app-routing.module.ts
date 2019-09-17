3315b52753a5bfd221b0d1dd1b1e2cd5796cd95b
Jahdai Reyes <jreyesortega@quaresitsolutions.com>
2019-09-17 10:26:09 -0300

    Form Group

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { TemplateComponent } from './components/template/template.component';
 import { DataComponent } from './components/data/data.component';
 import { ContactComponent } from './components/contact/contact.component';
 
 const routes: Routes = [
-  { path: 'template', component: TemplateComponent},
   { path: 'data', component: DataComponent},
   { path: 'contacto', component: ContactComponent}
 ];
